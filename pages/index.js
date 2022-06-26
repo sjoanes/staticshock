1fde49aa457aa5c95324d0e04c2867c0d9e99c59
Savio Joanes <joanes.savio@gmail.com>
2022-06-25 20:02:36 -0400

    feat: add sun and moon cycles

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,8 @@ export default function Home() {
     canvas.height = window.innerHeight;
 
     const hand = new Hand();
-    const leaves = Array.from(Array(50).keys()).map(() => new Leaves(hand));
+    const leaves = Array.from(Array(50).keys()).map(() => new Leaf(hand));
+    const sun = new Sun();
 
     canvas.addEventListener('mousemove', function(e) {
       hand.x = e.pageX;
@@ -34,12 +35,12 @@ export default function Home() {
       canvas.height = window.innerHeight;
     });
 
-    animator(leaves)();
+    animator(leaves, sun)();
   }, []);
   return <canvas id="canvas1" />
 }
 
-class Leaves {
+class Leaf {
   constructor(hand) {
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
@@ -103,19 +104,64 @@ class Hand {
   draw() {}
 }
 
+class Sun {
+  constructor() {
+    this.x = 0;
+    this.y = canvas.height / 2;
+    this.size = 100;
+  }
 
-function animator(leaves) {
+  update () {
+    const origin_x = 0;
+    const origin_y = canvas.height;
+    const ROTATE = Math.PI/400;
+
+    this.x = Math.cos(ROTATE) * (this.x - origin_x) - Math.sin(ROTATE) * (this.y - origin_y) + origin_x;
+    this.y = Math.sin(ROTATE) * (this.x - origin_x) + Math.cos(ROTATE) * (this.y - origin_y) + origin_y;
+  }
+
+  draw() {
+    // sun
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+    ctx.fill();
+
+    // moon
+    const origin_x = 0;
+    const origin_y = canvas.height;
+    const moon_x = Math.cos(Math.PI) * (this.x - origin_x) - Math.sin(Math.PI) * (this.y - origin_y) + origin_x;
+    const moon_y = Math.sin(Math.PI) * (this.x - origin_x) + Math.cos(Math.PI) * (this.y - origin_y) + origin_y;
+    const CRESCENT_OPENING = Math.PI/2;
+
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.arc(moon_x, moon_y, this.size, Math.PI*0.3, Math.PI * 1.56);
+    ctx.arc(moon_x + 40, moon_y - 10, this.size-10, Math.PI*1.42, Math.PI*0.42, true);
+    ctx.fill();
+    
+  }
+}
+
+
+function animator(leaves, sun) {
   return function animate() {
+    // sky
     var sky = ctx.createLinearGradient(0, canvas.height - 250, 0, 0);
     sky.addColorStop(0, "#8fd1ee");
     sky.addColorStop(1, "#1d97d8");
     ctx.fillStyle = sky;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    // name
     ctx.font = `80px Tahoma `;
     ctx.fillStyle = 'white';
     ctx.fillText("SAVIO", 10, 80);
 
+    sun.draw();
+    sun.update();
+
+    // beach
     ctx.fillStyle = '#f5f0d8';
     ctx.beginPath();
     ctx.moveTo(canvas.width/2, canvas.height);
@@ -123,7 +169,10 @@ function animator(leaves) {
     ctx.lineTo(canvas.width, canvas.height-100);
     ctx.fill();
     ctx.fillStyle = '#49bcb9cc';
+
+    // ocean
     ctx.fillRect(0, canvas.height-50, canvas.width, 50);
+
     for (const leaf of leaves) {
       leaf.update();
       leaf.draw();
